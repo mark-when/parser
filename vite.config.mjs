6e64a53b8b5fec719c9774cf09a454a52ede6a5f
Rob Koch <kochrt@gmail.com>
2024-07-02 15:36:32 -0700

    definition files and the like

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,13 +1,16 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dtsPlugin from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "./src/index.ts"),
-      name: "parser",
-      'formats': ['cjs']
+      name: "index",
+      fileName: "index",
+      formats: ["cjs"],
     },
     outDir: "cjs",
   },
+  plugins: [dtsPlugin({ rollupTypes: true })],
 });
