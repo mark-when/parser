e21924e68491980f20967b3c4e5bc6062e51e1c8
Rob Koch <kochrt@gmail.com>
2025-04-21 17:32:41 -0700

    timezones test

---
+++ tests/timezones.test.ts
@@ -0,0 +1,24 @@
+import { parse, toDateRange } from "../src";
+import { eventsWithTz } from "./testStrings";
+import { nthEvent } from "./testUtilities";
+
+describe("timezones", () => {
+  test("timezone by event property", () => {
+    const mw = parse(eventsWithTz);
+    const ny = nthEvent(mw, 0);
+    const tok = nthEvent(mw, 1);
+
+    expect(
+      toDateRange(ny.dateRangeIso)
+        .fromDateTime.diff(toDateRange(tok.dateRangeIso).fromDateTime)
+        .as("hours")
+    ).toBe(9 + 4);
+  });
+
+  test.only("timezone by group property", () => {
+    const mw = parse(eventsWithTz);
+    const la = nthEvent(mw, 2);
+
+    expect(toDateRange(la.dateRangeIso).fromDateTime.zone.name).toBe("");
+  });
+});
